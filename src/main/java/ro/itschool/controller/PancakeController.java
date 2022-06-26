e79d631cec6a0d43b67ce67f94935c34f5036132
AlexC3145 <comendantalex@gmail.com>
2022-06-26 12:53:16 +0300

    Search Pancake By Name

--- src/main/java/ro/itschool/controller/PancakeController.java
+++ src/main/java/ro/itschool/controller/PancakeController.java
@@ -1,18 +1,11 @@
 package ro.itschool.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ro.itschool.entity.Pancake;
 import ro.itschool.repository.PancakeRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class PancakeController {

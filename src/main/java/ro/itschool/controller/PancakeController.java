eb6792342e6e9b375220229d0df9a137c2b01c2f
vladiacob999 <vladiacob07@gmail.com>
2022-07-01 18:51:09 +0300

    Loggers

--- src/main/java/ro/itschool/controller/PancakeController.java
+++ src/main/java/ro/itschool/controller/PancakeController.java
@@ -1,6 +1,8 @@
 package ro.itschool.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +12,9 @@ import org.springframework.web.servlet.ModelAndView;
 import ro.itschool.entity.Pancake;
 import ro.itschool.repository.PancakeRepository;
 
+import java.util.List;
 
+@Log4j2
 @Controller
 public class PancakeController {
 
@@ -21,6 +25,7 @@ public class PancakeController {
     //---------------------------------- GET all pancakes ------------------------------
     @GetMapping("/pancakes")
     public String getAllPancakes(Model model) {
+        log.info("Getting all pancakes...");
         model.addAttribute("pancakes", pancakeRepository.findAll());
         return "allPancakes";
     }
@@ -32,7 +37,9 @@ public class PancakeController {
     //Daca n-ar fi pancake gol, n-am avea unde sa ne salvam valorile
     @GetMapping("/savePancake")
     public String greetingForm(Model model) {
+        log.info("Creating empty pancake...");
         model.addAttribute("pancakeObject", new Pancake());
+        log.info("Empty pancake created.");
         return "savePancake";
     }
 
@@ -40,31 +47,39 @@ public class PancakeController {
     //Pancake-ul vine populat din html
     @PostMapping("/savePancake")
     public String greetingSubmit(@ModelAttribute Pancake pancake, Model model) {
+        log.info("Submitting pancake...");
         model.addAttribute("pancakeObject", pancake);
         pancakeRepository.save(pancake);
+        log.info("Pancake was created");
         return "redirect:/pancakes";
+
     }
 
     //----------------------------Update---------------------------------------//
 
 
-
     @GetMapping("/updatePancake/{id}")
     public String getPancakeById(@PathVariable Integer id, Model model) {
+        log.info("Updating pancake with id : " + id);
         if (pancakeRepository.findById(id).isPresent()) {
             model.addAttribute("pancake", pancakeRepository.findById(id).get());
             return "updatePancake";
         }
+        log.info("Pancake was updated.");
         return ("Pancake not found for this id : " + id);
     }
 
 
-
     @RequestMapping(path = "/delete/{id}")
-    public String deletePancakeById(Model model, @PathVariable("id") Integer id)
-            throws Exception
-    {
+    public String deletePancakeById(Model model, @PathVariable("id") Integer id) throws Exception {
+        log.info("Deleting pancake with id: " + id + ".");
         pancakeRepository.deleteById(id);
+        log.info("Pancake was deleted.");
         return "redirect:/pancakes";
     }
+
+
+
+
+
 }
